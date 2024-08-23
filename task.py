21379ffa168ed7d7209bb2e5d7867b12659533a3
anveshSagi <31990935+anveshSagi@users.noreply.github.com>
2024-08-23 10:55:21 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,21 @@
+from prefect import task
+import httpx
+
+@task(retries=2)
+def get_repo_info(repo_owner: str, repo_name: str):
+    """Get info about a repo - will retry twice after failing"""
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}"
+    api_response = httpx.get(url)
+    api_response.raise_for_status()
+    repo_info = api_response.json()
+    return repo_info
+
+
+@task
+def get_contributors(repo_info: dict):
+    """Get contributors for a repo"""
+    contributors_url = repo_info["contributors_url"]
+    response = httpx.get(contributors_url)
+    response.raise_for_status()
+    contributors = response.json()
+    return contributors
