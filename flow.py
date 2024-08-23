39f24f8413a0d9642849fd0e90f63767c8754cd9
anveshSagi <31990935+anveshSagi@users.noreply.github.com>
2024-08-23 10:54:34 +0530

    Create flow.py

---
+++ flow.py
@@ -0,0 +1,21 @@
+from prefect import flow
+
+from task import get_contributors, get_repo_info
+
+
+
+@flow(log_prints=True)
+def repo_info(repo_owner: str = "PrefectHQ", repo_name: str = "prefect"):
+    """
+    Given a GitHub repository, logs the number of stargazers
+    and contributors for that repo.
+    """
+    repo_info = get_repo_info(repo_owner, repo_name)
+    print(f"Stars 🌠 : {repo_info['stargazers_count']}")
+
+    contributors = get_contributors(repo_info)
+    print(f"Number of contributors 👷: {len(contributors)}")
+
+
+if __name__ == "__main__":
+    repo_info()
